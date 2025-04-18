c105123bc063693ce020cd67bf549067a1ca1978
STEP3DLab <projects.step3d@gmail.com>
2025-04-19 00:31:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const { v4: uuidv4 } = require('uuid');
 const { Chess } = require('chess.js');
 
 const app = express();
@@ -17,16 +16,15 @@ io.on('connection', socket => {
   socket.on('join', ({ roomId }) => {
     socket.join(roomId);
     if (!games[roomId]) {
-      // new game
       games[roomId] = {
         chess: new Chess(),
         clocks: { w: 300000, b: 300000 },
         turn: 'w',
-        players: [],
+        players: []
       };
     }
     const game = games[roomId];
-    // assign role
+
     if (game.players.length < 2) {
       const role = game.players.length === 0 ? 'white' : 'black';
       game.players.push({ id: socket.id, role });
@@ -34,30 +32,23 @@ io.on('connection', socket => {
     } else {
       socket.data.role = 'spectator';
     }
-
     socket.data.roomId = roomId;
 
-    // send init
     io.to(socket.id).emit('init', {
       fen: game.chess.fen(),
       clocks: game.clocks,
       turn: game.turn,
-      role: socket.data.role,
+      role: socket.data.role
     });
   });
 
   socket.on('move', ({ roomId, move }) => {
     const game = games[roomId];
     if (!game) return;
-
     const result = game.chess.move(move);
     if (!result) return;
 
-    // switch turn
     game.turn = game.chess.turn();
-    // Note: clocks adjustment happens on client side
-
-    // broadcast
     io.in(roomId).emit('move', {
       fen: game.chess.fen(),
       clocks: game.clocks,
@@ -65,7 +56,7 @@ io.on('connection', socket => {
       gameOver: game.chess.game_over(),
       outcome: game.chess.in_checkmate()
         ? `${game.chess.turn() === 'w' ? 'Black' : 'White'} wins`
-        : null,
+        : null
     });
   });
 
@@ -78,12 +69,8 @@ io.on('connection', socket => {
     io.in(roomId).emit('rematch', {
       fen: game.chess.fen(),
       clocks: game.clocks,
-      turn: game.turn,
+      turn: game.turn
     });
-  });
-
-  socket.on('disconnect', () => {
-    // optional: handle reconnection or cleanup
   });
 });
 
